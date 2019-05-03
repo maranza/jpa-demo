6f65c4ee76d80a210a65fd3eaf9026a56215f4ed
maranza <o.maranza@gmail.com>
2019-05-03 23:50:42 +0200

    change repository to extend JpaRepository instead of CRUDRepository

--- src/main/java/org/coresystems/repositories/CompanyRepository.java
+++ src/main/java/org/coresystems/repositories/CompanyRepository.java
@@ -1,9 +1,9 @@
 package org.coresystems.repositories;
 
 import org.coresystems.models.Company;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CompanyRepository extends CrudRepository<Company, Integer> {
+public interface CompanyRepository extends JpaRepository<Company, Integer> {
 }
