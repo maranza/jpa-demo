9d49aea3dfbea4d556596f5fd97836224df10475
maranza <o.maranza@gmail.com>
2019-05-04 01:04:28 +0200

    reorder imports, refactor get all companies method and add company method

--- src/main/java/org/coresystems/services/CompanyService.java
+++ src/main/java/org/coresystems/services/CompanyService.java
@@ -1,19 +1,25 @@
 package org.coresystems.services;
 
-import java.util.List;
-
 import org.coresystems.models.Company;
 import org.coresystems.repositories.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class CompanyService {
 
 	@Autowired
 	private CompanyRepository companyRepository;
-	
+
 	public List<Company> getCompanies() {
-		return (List<Company>) companyRepository.findAll();
+		return new ArrayList<>(companyRepository.findAll());
 	}
+
+	public void addCompany(Company company) {
+		companyRepository.save(company);
+	}
+
 }
