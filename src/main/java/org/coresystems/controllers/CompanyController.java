5177b2ed7404c7cdaf3f2a96de5aa390c0a7648a
maranza <o.maranza@gmail.com>
2019-05-04 01:07:32 +0200

    reorder imports, refactor endpoint, implement the add company method

--- src/main/java/org/coresystems/controllers/CompanyController.java
+++ src/main/java/org/coresystems/controllers/CompanyController.java
@@ -1,16 +1,14 @@
 package org.coresystems.controllers;
 
-import java.util.List;
-
 import org.coresystems.models.Company;
 import org.coresystems.services.CompanyService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/company")
+import java.util.List;
+
 @RestController
+@RequestMapping("/companies")
 public class CompanyController {
 	
 	@Autowired
@@ -19,5 +17,10 @@ public class CompanyController {
 	@GetMapping("/all")
 	public List<Company> getCompanies() {
 		return companyService.getCompanies();
+	}
+	
+	@PostMapping("/add")
+	public void addCompany(@RequestBody Company company) {
+		companyService.addCompany(company);
 	}
 }
